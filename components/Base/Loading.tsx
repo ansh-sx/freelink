d191e5a2982a3fce72a6b61fbccbc16d7e6e2eb3
Ansh Sharma <147230122+ansh-sx@users.noreply.github.com>
2025-03-24 14:11:13 +0530

    Update Loading.tsx

--- components/Base/Loading.tsx
+++ components/Base/Loading.tsx
@@ -1,10 +1,13 @@
-// components/Spinner.tsx
 import React from 'react';
 
-const Spinner: React.FC = () => {
+interface SpinnerProps {
+  className?: string;
+}
+
+const Spinner: React.FC<SpinnerProps> = ({ className = '' }) => {
   return (
     <svg
-      className="animate-spin"
+      className={`animate-spin ${className}`}
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
