60fcd4db0b9076e2b21d59e111a92058340ebbb1
Ansh Sharma <147230122+ansh-sx@users.noreply.github.com>
2025-03-24 14:54:57 +0530

    Update Preview.tsx

--- components/AppForm/Preview.tsx
+++ components/AppForm/Preview.tsx
@@ -11,7 +11,7 @@ const PhonePreview: React.FC<PhonePreviewProps> = ({ data }) => {
   return (
     <div className="h-screen grid place-items-center">
       <div className="h-[729px] w-[340px] overflow-y-auto rounded-[3rem] ring-8 ring-slate-800 overflow-hidden">
-        <TemplatesSimple acc={data} />
+        <ProfileSection acc={data} />
       </div>
     </div>
   );
