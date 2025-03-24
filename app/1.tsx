4359a1f6113e00449b00b139261fb4ce708cb5cf
Ansh Sharma <147230122+ansh-sx@users.noreply.github.com>
2025-03-24 13:37:05 +0530

    Create 1.tsx

---
+++ app/1.tsx
@@ -0,0 +1,35 @@
+// pages/your-page.tsx
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+import TemplatesSimple from '@/components/templates/TemplatesSimple'
+import BaseLoading from '@/components/ui/BaseLoading'
+import { decodeData } from '@/utils/transformer'
+
+const YourPage = () => {
+  const router = useRouter()
+  const [decodedData, setDecodedData] = useState<any>(null)
+
+  useEffect(() => {
+    if (router.isReady) {
+      const acc = router.query.data
+      if (acc) {
+        const result = decodeData(acc as string)
+        setDecodedData(result)
+      }
+    }
+  }, [router.isReady, router.query.data])
+
+  return (
+    <div>
+      {decodedData ? (
+        <TemplatesSimple acc={decodedData} />
+      ) : (
+        <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+          <BaseLoading className="h-5 w-5" />
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default YourPage;
